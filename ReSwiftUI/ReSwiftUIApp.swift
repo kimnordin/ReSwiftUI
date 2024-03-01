9ab8f3862590e7dfe13b42a72c3dc4e7bf586628
Kim <nordinsane@gmail.com>
2024-03-01 13:27:08 +0100

    Initial Commit

---
+++ ReSwiftUI/ReSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReSwiftUIApp.swift
+//  ReSwiftUI
+//
+//  Created by Kim Nordin on 2024-03-01.
+//
+
+import SwiftUI
+
+@main
+struct ReSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
